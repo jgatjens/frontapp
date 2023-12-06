33ffdd94100ec05f2e4bceef2049a18feaba3676
jgatjens <jgatjens@gmail.com>
2023-12-06 13:10:14 -0300

    finally fix type warning on zod

--- src/app/[locale]/[applicationId]/pre-approved/(personal-info)/campaign-form.tsx
+++ src/app/[locale]/[applicationId]/pre-approved/(personal-info)/campaign-form.tsx
@@ -6,7 +6,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 // import type { ClientsType } from "~/server/db/org-schema";
 // import { api } from "~/trpc/react";
-import * as z from "zod";
+import type * as z from "zod";
 import { cn } from "@/lib/utils";
 
 import { buttonVariants } from "@/components/ui/button";
