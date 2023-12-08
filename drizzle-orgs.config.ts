1d8b66cb1fd72735172593d867504e6d4602c1bc
jgatjens <jgatjens@gmail.com>
2023-12-08 17:13:25 -0300

    remove dotenv call

--- drizzle-orgs.config.ts
+++ drizzle-orgs.config.ts
@@ -1,10 +1,5 @@
 import type { Config } from "drizzle-kit";
-// import { env } from "~/env.mjs";
 
-import * as dotenv from "dotenv";
-dotenv.config();
-
-// console.log("process.env", process.env);
 export default {
   schema: "./src/server/db/schema-orgs.ts",
   schemaFilter: ["company_abc", "company_zxy"],
